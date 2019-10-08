dbc7817bee81cc77626044bda18c83c0c15c80d7
missasd <missasd9@hotmai.com>
2019-10-08 15:47:13 +0800

    first commit

---
+++ src/sw/day08/demo01/exe1_1_1.java
@@ -0,0 +1,12 @@
+package sw.day08.demo01;
+
+public class exe1_1_1 {
+    public static void main(String[] args) {
+        double a = ( 0 + 15) / 2;
+        int b = (0+15) / 2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(2.0e-6 * 100000000.1);
+        System.out.println(true && false || true && true);
+    }
+}
